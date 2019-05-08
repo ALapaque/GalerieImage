dc3ff7086d2555a517651d4866e763e465fe41c2
Amaury <amaury.lapaque@gmail.com>
2019-05-08 13:37:34 +0200

    First commit

---
+++ src/main/java/be/tftic/galerieimage/GalerieimageApplication.java
@@ -0,0 +1,13 @@
+package be.tftic.galerieimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalerieimageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalerieimageApplication.class, args);
+    }
+
+}
