3af9d47bfaa9447fdae193101b407c2607c4d80f
Amaury <amaury.lapaque@gmail.com>
2019-05-15 10:20:22 +0200

    /uploadFile -> file mit en header /downloadFile/id du fichier WORKS stored in a BLOB

--- src/main/java/be/tftic/galerieimage/controllers/FileController.java
+++ src/main/java/be/tftic/galerieimage/controllers/FileController.java
@@ -3,6 +3,7 @@ package be.tftic.galerieimage.controllers;
 import be.tftic.galerieimage.dal.entitites.DBFile;
 import be.tftic.galerieimage.dal.services.DBFileStorageService;
 import be.tftic.galerieimage.payload.UploadFileResponse;
+import org.apache.catalina.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,6 +30,7 @@ public class FileController {
     @PostMapping("/uploadFile")
     public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
         DBFile dbFile = DBFileStorageService.storeFile(file);
+
 
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/downloadFile/")
